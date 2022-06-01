66c2de113febbdda7982c6a5cd411c5f61ed9819
Sabine Detering <sabine.detering@web.de>
2022-06-01 23:02:44 +0200

    first test cases cypress

--- cypress/integration/dogschool-crm-spec.js
+++ cypress/integration/dogschool-crm-spec.js
@@ -4,3 +4,26 @@ describe('start screen', () => {
     })
 })
 
+it('login as guest', () => {
+    cy.visit('/')
+    cy.get('[data-cy=guestLogin]')
+        .click()
+    cy.contains('Sign in')
+        .click()
+    cy.location().should((location) => {
+        expect(location.pathname).to.eq('/dashboard')
+    })
+})
+
+describe('client page', () => {
+    it('successfully loads', () => {
+        cy.visit('/clients')
+    })
+})
+
+it('create new client', () => {
+    cy.visit('/clients')
+    cy.get('[data-cy=addClient]').click()
+    cy.get('h2').should('contain','Add new client')
+    
+})
