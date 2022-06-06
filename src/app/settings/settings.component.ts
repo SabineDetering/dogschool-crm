ab0a1be75c06e7c789ed7edfa8bdefcb619a06ed
Sabine Detering <sabine.detering@web.de>
2022-06-06 16:33:56 +0200

    adding new subject, discarding changes of subjects implemented

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -11,6 +11,7 @@ import { DataService } from 'src/services/data.service';
 export class SettingsComponent implements OnInit {
 
   subjects: string[];
+  newSubject: string='';
 
   constructor(public Data: DataService) { }
   
@@ -20,12 +21,32 @@ export class SettingsComponent implements OnInit {
   }
 
 
+  /**
+   * moves the dragged item into the new position in the array
+   * @param event 
+   */
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.subjects, event.previousIndex, event.currentIndex);
-    console.log(this.subjects);
   }
 
+
+  addSubject() {
+    this.subjects.unshift(this.newSubject.trim());
+    this.newSubject = '';
+   }
+  
+
+  deleteSubject(i:number) {
+    this.subjects.splice(i, 1);
+  }
+
+
   saveSubjects() {
+    console.log('saved subjects', this.subjects);
     this.Data.saveSubjects(this.subjects);
+  }
+
+  discardChanges() {
+    window.location.reload();
   }
 }
